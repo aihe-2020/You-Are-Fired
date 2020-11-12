57e2bcc53fdbbaa9fdd46338b24fb2af11d03df9
Rowan-Potter <rowan@DESKTOP-04CLBHG.localdomain>
2020-11-12 17:02:40 +1300

    Initial Refactoring

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <h1>You're Fired!</h1>
   )
 }
+
+class App extends React.Component {
+  
+}
+
+
 
 export default App
