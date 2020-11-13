a27e7867150ca378a0ee4a779253ea0314626699
sarah-knowles <sjk339@gmail.com>
2020-11-13 13:48:43 +1300

    further styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 
-import { getDonQuote,getKanyeQuote } from '../api'
+import { getDonQuote, getKanyeQuote } from '../api'
 
 import TrumpImage from './TrumpImage'
 import KanyeImage from './KanyeImage'
@@ -15,55 +15,61 @@ class App extends React.Component {
 
   state = {
 
-   donQuote:'',
-   kanyeQuote:''
+    donQuote: '',
+    kanyeQuote: ''
 
 
-   
+
 
   }
-  componentDidMount () {
+  componentDidMount() {
 
     // let donQuote = getQuote()
     getDonQuote()
-    .then(res => {this.setState({donQuote: res.value})})
+      .then(res => { this.setState({ donQuote: res.value }) })
     // .then(res =>{this.setState({donQuote:res.value})})
     getKanyeQuote()
-      .then(res => {this.setState({kanyeQuote:res.quote})})
-      console.log(this.state)
-      // .then(res =>{this.setState({kanyeQuote:res.value})})
-    }
-      // advice()
-      // .then(res => this .setState({advice: res.advice}))
+      .then(res => { this.setState({ kanyeQuote: res.quote }) })
+    console.log(this.state)
+    // .then(res =>{this.setState({kanyeQuote:res.value})})
+  }
+  // advice()
+  // .then(res => this .setState({advice: res.advice}))
 
 
-      // getKanyeQuote()
-      // .then(res => {this.setState({ kanyeQuote: res.value })
-      // }):kanyeQuote
-      // getKanyeQuote()
-      // .then(res =>{this.setState({kanyeQuote:res.value})})
+  // getKanyeQuote()
+  // .then(res => {this.setState({ kanyeQuote: res.value })
+  // }):kanyeQuote
+  // getKanyeQuote()
+  // .then(res =>{this.setState({kanyeQuote:res.value})})
 
 
 
   render() {
 
-    return(
+    return (<div>
+      <h1>You're Fired!</h1>
       <div class='wrapper'>
-      <h1>You're Fired!</h1>   
 
-      <div class='card'>
-     <h4>{this.state.donQuote}</h4>
-     <TrumpImage />
-     </div>
-     <div class ='card'>
-     <h4>****{this.state.kanyeQuote}</h4>
-     <KanyeImage />
-     
-     </div>
-     
 
-     </div>
+        <div class='card'>
+          <h4>{this.state.donQuote}</h4>
+          <div class='image'>
+            <TrumpImage />
+          </div>
+          <button>More Trump!</button>
+        </div>
+        <div class='card'>
+          <h4>{this.state.kanyeQuote}</h4>
+          <div class='image'>
+            <KanyeImage />
+          </div>
+          <button>More Kanye!</button>
+        </div>
 
+
+      </div>
+    </div>
     )
   }
 }
